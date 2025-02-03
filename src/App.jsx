fc89a6fb4da3148f9339adfba894903ea4a59ab4
Lyxrae <nelsonyfelix23@gmail.com>
2025-02-03 19:31:48 +0300

    Tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+     <p class="text-black">Tailwind is awesome</p>
+     <p class="text-white">Tailwind is awesome</p>
+     <p class="text-red-900">Tailwind is awesome</p>
+     <p class="text-green-900">Tailwind is awesome</p>
+     <p class="text-zink-900">Tailwind is awesome</p>
+     <p class="text-emerald-900">Tailwind is awesome</p>
+     <p class="text-lime-900">Tailwind is awesome</p>
+
+  
+
+    <p class="bg-teal-200">Tailwind is awesome</p>
+     <p class="bg-blue-600 text-orange-300">Tailwind is awesome</p>
+     <p class="bg-red-500">Tailwind is awesome</p>
+     <p class="bg-zink-400">Tailwind is awesome</p>
+     <p class="bg-emerald-400">Tailwind is awesome</p>
+     <p class="bg-rose-900 text-white">Tailwind is awesome</p>
+
+  
+    <p class="underline decoration-red-400">Tailwind is awesome</p>
+
+
+    
+     <input class="border border-blue-400" type="text" />
+     <input class="border border-orange-400" type="text" />
+
+
+    
+     <div class="divide-y divide-blue-400">
+      <div>Item</div>
+      <div>Item2</div>
+      <div>Item3</div>
+      <div>Item4</div>
+
+     </div>
+
+    
+
+    <button class="outline outline-blue-300">Submit form </button>
+
+  
+     <div class="shadow-lg bg-cyan-500 shadow-cyan-500">
+       Subscrible here
+     </div>
+
+     <button class="shadow-lg bg-cyan-500 shadow-purple-500/60"> Hello Shadow </button>
+
+    
+
+    <input class="accent-purple-400" type="checkbox" checked />
+
+    
+     <div class="bg-[#427fab]">Hello</div>
+
+    </div>
+  )
+}
+
+export default App
